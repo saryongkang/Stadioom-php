05d185a9dcdaa479f037feaa558b4923449f4520
Wegra <wegra@seedshock.com>
2011-10-01 12:36:07 +0900

    Updated the ssl_helper.php to support 8080 port (for local testing).

--- application/helpers/ssl_helper.php
+++ application/helpers/ssl_helper.php
@@ -17,7 +17,7 @@ function remove_ssl() {
     $CI = & get_instance();
     $CI->config->config['base_url'] =
             str_replace('https://', 'http://', $CI->config->config['base_url']);
-    if ($_SERVER['SERVER_PORT'] != 80) {
+    if ($_SERVER['SERVER_PORT'] != 80 && $_SERVER['SERVER_PORT'] != 8080) {
         redirect($CI->uri->uri_string());
     }
 }
