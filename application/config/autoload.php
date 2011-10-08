9f5c36910912d4b46eb20ddbd12280f587a247e2
Norú Moreno <noruisc@gmail.com>
2011-10-08 18:27:48 +0900

    Autoloding facebook configs

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -80,7 +80,7 @@ $autoload['helper'] = array('url', 'ssl_helper');
 |
 */
 
-$autoload['config'] = array();
+$autoload['config'] = array('facebook');
 
 
 /*
--- application/config/facebook.php
+++ application/config/facebook.php
@@ -1,6 +1,10 @@
 <?php
 
+//$config['appId'] = '200987663288876';
+//$config['secret'] = '6d3dd0e7aa9dae300920ec05552bddee';
+
+
+
 $config['appId'] = '205626906162291';
 $config['secret'] = '54dbf27c45e7f4f31150f925fbb18065';
-
 ?>
