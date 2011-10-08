b39e896bb69c3daeedb45673dd4c76b7aeb16723
Norú Moreno <noruisc@gmail.com>
2011-10-08 18:27:01 +0900

    Made own deploying for server with Facebook dev and prod

---
+++ application/config/facebook_prod.php
@@ -0,0 +1,6 @@
+<?php
+
+$config['appId'] = '200987663288876';
+$config['secret'] = '6d3dd0e7aa9dae300920ec05552bddee';
+
+?>
---
+++ deployNoru.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+cd ~
+
+if [ -f dev.pem ];
+then
+
+BASE_DIR=/Applications/MAMP/htdocs/Stadioom-php
+
+cp $BASE_DIR/index_prod.php $BASE_DIR/index.php
+cp $BASE_DIR/application/doctrine-cli_prod.php $BASE_DIR/application/doctrine-cli.php
+cp $BASE_DIR/application/config/config_prod.php $BASE_DIR/application/config/config.php
+cp $BASE_DIR/application/config/facebook_prod.php $BASE_DIR/application/config/facebook.php
+
+rsync -Paz --rsh "ssh -i ./dev.pem" --rsync-path "sudo rsync" --exclude 'user_guide' --exclude '*.git' $BASE_DIR/ ubuntu@107.20.209.139:/var/www/
+
+cp $BASE_DIR/index_dev.php $BASE_DIR/index.php
+cp $BASE_DIR/application/doctrine-cli_dev.php $BASE_DIR/application/doctrine-cli.php
+cp $BASE_DIR/application/config/config_dev.php $BASE_DIR/application/config/config.php
+cp $BASE_DIR/application/config/facebook_dev.php $BASE_DIR/application/config/facebook.php
+
+
+else
+
+echo "ERROR: The dev.pem file should be placed at your HOME directory."
+cd -
+exit 1
+
+fi
+
+cd -
+exit 0
