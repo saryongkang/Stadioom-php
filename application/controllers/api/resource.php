abaad12a2d55603fb6e07bf0fd036d8ee7533af5
Wegra <wegra@seedshock.com>
2011-10-09 05:36:34 +0900

    Added 'resource' APIs

---
+++ application/controllers/api/resource.php
@@ -0,0 +1,134 @@
+<?php
+
+require(APPPATH . '/libraries/Stadioom_REST_Controller.php');
+
+class Resource extends Stadioom_REST_Controller {
+
+    function __construct() {
+        parent::__construct();
+
+        $this->load->model('dao/ResourceDao');
+
+        force_ssl();
+    }
+
+//    public function getMessage($clientType) {
+////        $accessToken = $this->get('accessToken');
+//        try {
+////            $userId = $this->verifyToken($accessToken);
+//
+//            $lang = $this->get("lang");
+//            if ($lang == null) {
+//                $lang = "en_US";
+//            }
+//
+//            $msgId = $this->get("numId");
+//            if ($msgId != null) {
+////                echo "<html><head><meta http-equiv ='Content-Type' content='text/html; charset=utf-8'><body>";
+////                echo $this->ResourceDao->getMessageByMsgId($msgId, $clientType, $lang);
+//                $this->responseOk(array('msg' => $this->ResourceDao->getByNumId($msgId, $clientType, $lang)));
+////                echo "</body></head></html>";
+//            }
+//            $msgId = $this->get("strId");
+//            if ($msgId != null) {
+//                $this->responseOk(array('msg' => $this->ResourceDao->getByStrId($msgId, $clientType, $lang)));
+//            }
+//            throw new Exception("'numId' or 'strId' is required.", 400);
+//        } catch (Exception $e) {
+//            $this->responseError($e);
+//        }
+//    }
+//    public function getResource($clientType) {
+////        $accessToken = $this->get('accessToken');
+//        try {
+////            $userId = $this->verifyToken($accessToken);
+//
+//            $lang = "en_US";
+//
+//            $msgId = $this->get("numId");
+//            if ($msgId != null) {
+////                echo "<html><head><meta http-equiv ='Content-Type' content='text/html; charset=utf-8'><body>";
+////                echo $this->ResourceDao->getMessageByMsgId($msgId, $clientType, $lang);
+//                $this->responseOk(array('msg' => $this->ResourceDao->getByNumId($msgId, $clientType, $lang)));
+////                echo "</body></head></html>";
+//            }
+//            $msgId = $this->get("strId");
+//            if ($msgId != null) {
+//                $this->responseOk(array('msg' => $this->ResourceDao->getByStrId($msgId, $clientType, $lang)));
+//            }
+//            throw new Exception("'numId' or 'strId' is required.", 400);
+//        } catch (Exception $e) {
+//            $this->responseError($e);
+//        }
+//    }
+//    
+    public function index_get() {
+//        $accessToken = $this->get('accessToken');
+        try {
+//            $userId = $this->verifyToken($accessToken);
+            $lang = $this->get("lang");
+            if ($lang == null) {
+                $lang = "en_US";
+            }
+
+            $clientType = $this->get("clientType");
+
+            $id = $this->get('id');
+            if (is_numeric($id)) {
+                $this->responseOk(array('msg' => $this->ResourceDao->getByNumId($id, $clientType, $lang)));
+            } else {
+                $this->responseOk(array('msg' => $this->ResourceDao->getByStrId($id, $clientType, $lang)));
+            }
+        } catch (Exception $e) {
+            $this->responseError($e);
+        }
+    }
+
+    public function delta_get($after) {
+        // TODO later...
+    }
+
+//    public function msg_get($id) {
+//        if (is_numeric($id)) {
+//            return $this->getMessageByNumId($id);
+//        } else {
+//            return $this->getMessageByStrId($id);
+//        }
+//    }
+//
+//    public function img_get($clientType) {
+//        return $this->getResource($clientType);
+//    }
+//
+//    public function link_get($clientType) {
+//        return $this->getResource($clientType);
+//    }
+//
+//    public function ios_get() {
+//        return $this->getMessage("ios");
+//    }
+//
+//    public function js_get() {
+//        return $this->getMessage("js");
+//    }
+    // only for testing purpose.
+    public function insert_get() {
+        $this->ResourceDao->insert(1000, 'img_shareicon', "http://stadioom.com/assets/images/sponsors/shareicons/{1}_{2}_shareicon.gif", "http://stadioom.com/assets/images/sponsors/shareicons/%1@_%2@_shareicon.gif", "http://stadioom.com/assets/images/sponsors/shareicons/%1s_%2s_shareicon.gif", "en_US");
+        $this->ResourceDao->insert(2000, 'link_match', "http://stadioom.com/match/view/{1}", "http://stadioom.com/match/view/%@", "http://stadioom.com/match/view/%s", "en_US");
+        $this->ResourceDao->insert(10001, 'msg_match_won', "{1} won!", "%@ won!", "%1s won!", "en_US");
+        $this->ResourceDao->insert(10001, 'msg_match_won', "{1} 승!", "%@ 승", "%1s 승!", "ko_KR");
+        $this->ResourceDao->insert(10002, 'msg_match_lost', "{1} lost!", "%@ lost!", "%1s lost!", "en_US");
+        $this->ResourceDao->insert(10002, 'msg_match_lost', "{1} 패!", "%@ 패!", "%1s 패!", "ko_KR");
+        $this->ResourceDao->insert(10003, 'msg_match_drew', "{1} drew!", "%@ drew!", "%1s drew!", "en_US");
+        $this->ResourceDao->insert(10003, 'msg_match_drew', "{1} 무승부!", "%@ 무승부!", "%1s 무승부!", "ko_KR");
+        $this->ResourceDao->insert(10004, 'msg_match_caption', "{1} defeated {2} in a fierce {3} match.", "%1@ defeated %2@ in a fierce %3@ match.", "%1s defeated %2s in a fierce %3s match.", "en_US");
+        $this->ResourceDao->insert(10004, 'msg_match_caption', "{1}가 {3} 경기에서 {2}에 대항하여 승리했습니다.", "%1@가 %3@ 경기에서 %2@에 대항하여 승리했습니다.", "%1s가 %3s 경기에서 %2s에 대항하여 승리했습니다.", "ko_KR");
+        $this->ResourceDao->insert(10005, 'msg_match_title', "{1} {2} Match", "%1@ %2@ Match", "%1s %2s Match", "en_US");
+        $this->ResourceDao->insert(10005, 'msg_match_title', "{1}배 {2}경기", "%1@배 %2@경기", "%1s배 %2s경기", "ko_KR");
+        $this->ResourceDao->insert(10006, 'msg_match_score', "Final score: {1} {2} - {3} {4}", "Final score: %1@ %2@ - %3@ %4@", "Final score: %1s %2s - %3s %4s", "en_US");
+        $this->ResourceDao->insert(10006, 'msg_match_score', "최종 점수: {1} {2} - {3} {4}", "최종 점수: %1@ %2@ - %3@ %4@", "최종 점수: %1s %2s - %3s %4s", "ko_KR");
+    }
+
+}
+
+?>
---
+++ application/models/Entities/Resource.php
@@ -0,0 +1,215 @@
+<?php
+
+namespace Entities;
+
+/**
+ * Entities\Resource
+ */
+class Resource
+{
+    /**
+     * @var integer $id
+     */
+    private $id;
+
+    /**
+     * @var integer $numId
+     */
+    private $numId;
+
+    /**
+     * @var string $strId
+     */
+    private $strId;
+
+    /**
+     * @var string $msg
+     */
+    private $msg;
+
+    /**
+     * @var string $msgiOS
+     */
+    private $msgiOS;
+
+    /**
+     * @var string $msgJS
+     */
+    private $msgJS;
+
+    /**
+     * @var string $lang
+     */
+    private $lang;
+
+    /**
+     * @var datetime $lastUpdated
+     */
+    private $lastUpdated;
+
+
+    /**
+     * Get id
+     *
+     * @return integer $id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set numId
+     *
+     * @param integer $numId
+     */
+    public function setNumId($numId)
+    {
+        $this->numId = $numId;
+    }
+
+    /**
+     * Get numId
+     *
+     * @return integer $numId
+     */
+    public function getNumId()
+    {
+        return $this->numId;
+    }
+
+    /**
+     * Set strId
+     *
+     * @param string $strId
+     */
+    public function setStrId($strId)
+    {
+        $this->strId = $strId;
+    }
+
+    /**
+     * Get strId
+     *
+     * @return string $strId
+     */
+    public function getStrId()
+    {
+        return $this->strId;
+    }
+
+    /**
+     * Set msg
+     *
+     * @param string $msg
+     */
+    public function setMsg($msg)
+    {
+        $this->msg = $msg;
+    }
+
+    /**
+     * Get msg
+     *
+     * @return string $msg
+     */
+    public function getMsg()
+    {
+        return $this->msg;
+    }
+
+    /**
+     * Set msgiOS
+     *
+     * @param string $msgiOS
+     */
+    public function setMsgiOS($msgiOS)
+    {
+        $this->msgiOS = $msgiOS;
+    }
+
+    /**
+     * Get msgiOS
+     *
+     * @return string $msgiOS
+     */
+    public function getMsgiOS()
+    {
+        return $this->msgiOS;
+    }
+
+    /**
+     * Set msgJS
+     *
+     * @param string $msgJS
+     */
+    public function setMsgJS($msgJS)
+    {
+        $this->msgJS = $msgJS;
+    }
+
+    /**
+     * Get msgJS
+     *
+     * @return string $msgJS
+     */
+    public function getMsgJS()
+    {
+        return $this->msgJS;
+    }
+
+    /**
+     * Set lang
+     *
+     * @param string $lang
+     */
+    public function setLang($lang)
+    {
+        $this->lang = $lang;
+    }
+
+    /**
+     * Get lang
+     *
+     * @return string $lang
+     */
+    public function getLang()
+    {
+        return $this->lang;
+    }
+
+    /**
+     * Set lastUpdated
+     *
+     * @param datetime $lastUpdated
+     */
+    public function setLastUpdated($lastUpdated)
+    {
+        $this->lastUpdated = $lastUpdated;
+    }
+
+    /**
+     * Get lastUpdated
+     *
+     * @return datetime $lastUpdated
+     */
+    public function getLastUpdated()
+    {
+        return $this->lastUpdated;
+    }
+    /**
+     * @prePersist
+     */
+    public function prePersist()
+    {
+        // Add your code here
+    }
+
+    /**
+     * @preUpdate
+     */
+    public function preUpdate()
+    {
+        // Add your code here
+    }
+}
