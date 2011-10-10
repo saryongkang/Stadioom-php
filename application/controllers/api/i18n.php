4093846d3a6afe72a866dfd62e035f3c58f7b4c5
Wegra <wegra@seedshock.com>
2011-10-11 00:31:00 +0900

    Removed some legacy code.

--- application/controllers/api/i18n.php
+++ application/controllers/api/i18n.php
@@ -7,14 +7,14 @@ class I18n extends Stadioom_REST_Controller {
     function __construct() {
         parent::__construct();
 
-        $this->load->model('dao/I18nDao2');
+        $this->load->model('dao/I18nDao');
 
         force_ssl();
     }
 
     public function trans_get() {
         try {
-            $this->responseOk($this->I18nDao2->translate($this->get('id'), $this->get("lang"), $this->get("clientType")));
+            $this->responseOk($this->I18nDao->translate($this->get('id'), $this->get("lang"), $this->get("clientType")));
         } catch (Exception $e) {
             $this->responseError($e);
         }
@@ -26,7 +26,7 @@ class I18n extends Stadioom_REST_Controller {
             $lang = $this->get('lang');
             $after = $this->get('after');
 
-            return $this->responseOk($this->I18nDao2->getDelta($category, $lang, $after));
+            return $this->responseOk($this->I18nDao->getDelta($category, $lang, $after));
         } catch (Exception $e) {
             $this->responseError($e);
         }
@@ -34,9 +34,9 @@ class I18n extends Stadioom_REST_Controller {
 
     public function lang_get($id = null) {
         if ($id == null) {
-            $this->responseOk(array('data' => $this->I18nDao2->getAllSupportedLanguages()));
+            $this->responseOk(array('data' => $this->I18nDao->getAllSupportedLanguages()));
         } else {
-            $name = $this->I18nDao2->getLanguageName($id);
+            $name = $this->I18nDao->getLanguageName($id);
             if ($name === null) {
                 $name = 'Not supported';
             }
