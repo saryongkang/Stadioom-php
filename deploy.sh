417bf278ca5de6118edc0855a5c8f6bdb599a15a
Wegra <wegra@seedshock.com>
2011-10-11 04:10:10 +0900

    Added 'facebook.php' auto-changing in deploy.sh file.

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,7 @@ cp $BASE_DIR/index_prod.php $BASE_DIR/index.php
 cp $BASE_DIR/application/doctrine-cli_prod.php $BASE_DIR/application/doctrine-cli.php
 cp $BASE_DIR/application/config/config_prod.php $BASE_DIR/application/config/config.php
 cp $BASE_DIR/application/config/database_prod.php $BASE_DIR/application/config/database.php
+cp $BASE_DIR/application/config/facebook_prod.php $BASE_DIR/application/config/facebook.php
 
 rsync -Paz --rsh "ssh -i ./dev.pem" --rsync-path "sudo rsync" --exclude 'user_guide' --exclude '*.git' $BASE_DIR/ ubuntu@107.20.209.139:/var/www/
 
@@ -18,6 +19,7 @@ cp $BASE_DIR/index_dev.php $BASE_DIR/index.php
 cp $BASE_DIR/application/doctrine-cli_dev.php $BASE_DIR/application/doctrine-cli.php
 cp $BASE_DIR/application/config/config_dev.php $BASE_DIR/application/config/config.php
 cp $BASE_DIR/application/config/database_dev.php $BASE_DIR/application/config/database.php
+cp $BASE_DIR/application/config/facebook_dev.php $BASE_DIR/application/config/facebook.php
 
 else
 
