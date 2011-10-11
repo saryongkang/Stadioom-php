d859e42e9bea4db7257f799b727e3e84aba382c7
Wegra <wegra@seedshock.com>
2011-10-12 01:02:01 +0900

    Changed the CI log path to /tmp/stadioom_logs/.

--- application/config/config_prod.php
+++ application/config/config_prod.php
@@ -195,7 +195,7 @@ $config['log_threshold'] = 2;
   | application/logs/ folder. Use a full server path with trailing slash.
   |
  */
-$config['log_path'] = '';
+$config['log_path'] = '/tmp/stadioom_logs/';
 
 /*
   |--------------------------------------------------------------------------
