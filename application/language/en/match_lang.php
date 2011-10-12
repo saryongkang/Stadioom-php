63952df10ee51421855f030dcddc3f7350d81e8e
Wegra <wegra@seedshock.com>
2011-10-12 17:44:43 +0900

    Updated I18n keys and languages.

--- application/language/en/match_lang.php
+++ application/language/en/match_lang.php
@@ -1,12 +1,18 @@
 <?php
 
-$lang['match_publish_won'] = "%1s won!";
-$lang['match_publish_lost'] = "%1s lost!";
-$lang['match_publish_drew'] = "%1s drew!";
-$lang['match_publish_caption'] = "%1s defeated %2s in a fierce %3s match.";
-$lang['match_publish_title'] = "%1s %2s Match";
-$lang['match_publish_score'] = "Final score: %1s %2s - %3s %4s";
-$lang['match_publish_link'] = "http://stadioom.com/match/view/%s";
-$lang['match_publish_icon'] = "http://stadioom.com/assets/images/sponsors/shareicons/%1s_%2s_shareicon.gif";
+$lang['match_fbmessage_won_singular'] = "I won!";
+$lang['match_fbmessage_won_plural'] = "We won!";
+$lang['match_fbmessage_lost_singular'] = "Great game!";
+$lang['match_fbmessage_lost_plural'] = "Great game!";
+$lang['match_fbmessage_tied_singular'] = "Great game!";
+$lang['match_fbmessage_tied_plural'] = "Great game!";
+
+$lang['match_fbmessage_caption_singlular'] = "%1s defeated %2s in a fierce %3s match.";
+$lang['match_fbmessage_caption_plural'] = "%1s defeated %2s in a fierce %3s match.";
+
+$lang['match_fbmessage_title'] = "%1s %2s Match";
+$lang['match_fbmessage_score'] = "Final score: %1s %2s - %3s %4s";
+$lang['match_fbmessage_link'] = "http://stadioom.com/match/view/%s";
+$lang['match_fbmessage_icon'] = "http://stadioom.com/assets/images/sponsors/shareicons/%1s_%2s_shareicon.gif";
 
 $lang['__last_updated'] = '1318234635';
