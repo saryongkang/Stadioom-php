e9935a16dc18bcd0b4aada6e89365b114eb6ed91
Norú Moreno <noruisc@gmail.com>
2011-10-13 03:41:10 +0900

    Corrected language messages structure to work properly with sprintf in php and jacasript

--- application/language/en/fbmessage_lang.php
+++ application/language/en/fbmessage_lang.php
@@ -5,15 +5,24 @@ $lang['fbmessage_won_singular'] = "I won!";
 $lang['fbmessage_won_plural'] = "We won the match!";
 $lang['fbmessage_lost_singular'] = "I lost the match...";
 $lang['fbmessage_lost_plural'] = "We lost...";
-$lang['fbmessage_tied_singular'] = "The match ended in tie";
+$lang['fbmessage_tied_singular'] = "The match ended in a tie";
 $lang['fbmessage_tied_plural'] = "We had a tied match";
 
-$lang['fbmessage_caption_singlular'] = "%1s defeated %2s in a fierce %3s match.";
-$lang['fbmessage_caption_plural'] = "%1s defeated %2s in a fierce %3s match.";
+$lang['fbmessage_caption_won_singular'] = "%1\$s defeated %2\$s in a fierce %3\$s match.";
+$lang['fbmessage_caption_won_plural'] = "%1\$s defeated %2\$s in a fierce %3\$s match.";
 
-$lang['fbmessage_title'] = "%1s %2s Match";
-$lang['fbmessage_score'] = "Final score: %1s %2s - %3s %4s";
-$lang['fbmessage_link'] = "match/view/%1s";
-$lang['fbmessage_icon'] = "assets/images/sponsors/shareicons/%1s_%2s_shareicon.gif";
+$lang['fbmessage_caption_lost_singular'] = "%1\$s was defeated by %2\$s in a fierce %3\$s match.";
+$lang['fbmessage_caption_lost_plural'] = "%1\$s were defeated by  %2\$s in a fierce %3\$s match.";
+
+$lang['fbmessage_caption_tied_singular'] = "%1\$s tied %2\$s in a fierce %3\$s match.";
+$lang['fbmessage_caption_tied_plural'] = "%1\$s tied %2\$s in a fierce %3\$s match.";
+
+$lang['fbmessage_andOthers'] = "%1\$s & %2\$d+";
+
+
+$lang['fbmessage_title'] = "%1\$s %2\$s match";
+$lang['fbmessage_score'] = "Final score: %1\$s %2\$s - %3\$s %4\$s";
+$lang['fbmessage_link'] = "match/view/%s";
+$lang['fbmessage_icon'] = "assets/images/sponsors/shareicons/%1\$s_%2\$s_shareicon.gif";
 
 $lang['__last_updated'] = '1318234635';
--- application/language/es/fbmessage_lang.php
+++ application/language/es/fbmessage_lang.php
@@ -8,12 +8,14 @@ $lang['fbmessage_lost_plural'] = "Perdimos...";
 $lang['fbmessage_tied_singular'] = "El partido quedó empatado";
 $lang['fbmessage_tied_plural'] = "Empatamos";
 
-$lang['fbmessage_caption_singular'] = "%1s venció a %2s en un intenso partido de %3s.";
-$lang['fbmessage_caption_plural'] = "%1s vencieron a %2s en un intenso partido de %3s.";
+$lang['fbmessage_caption_singular'] = "%1\$s venció a %2\$s en un intenso partido de %3\$s.";
+$lang['fbmessage_caption_plural'] = "%1\$s vencieron a %2\$s en un intenso partido de %3\$s.";
 
-$lang['fbmessage_title'] = "Partido de %1s %2s";
-$lang['fbmessage_score'] = "Puntuación final: %1s %2s - %3s %4s";
+
+$lang['fbmessage_title'] = "Partido de %1\$s %2\$s";
+$lang['fbmessage_score'] = "Puntuación final: %1\$s %2\$s - %3\$s %4\$s";
 $lang['fbmessage_link'] = "match/view/%1s";
-$lang['fbmessage_icon'] = "assets/images/sponsors/shareicons/%1s_%2s_shareicon.gif";
+$lang['fbmessage_icon'] = "assets/images/sponsors/shareicons/%1\$s_%2\$s_shareicon.gif";
+
 
 $lang['__last_updated'] = '1318234635';
