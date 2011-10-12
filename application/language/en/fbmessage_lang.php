c4ba2204769005d2f6850f97315c28df7e195fe6
Wegra <wegra@seedshock.com>
2011-10-12 23:43:11 +0900

    Bug Fix: errata in the fbmessage_link message for ios.

--- application/language/en/fbmessage_lang.php
+++ application/language/en/fbmessage_lang.php
@@ -13,7 +13,7 @@ $lang['fbmessage_caption_plural'] = "%1s defeated %2s in a fierce %3s match.";
 
 $lang['fbmessage_title'] = "%1s %2s Match";
 $lang['fbmessage_score'] = "Final score: %1s %2s - %3s %4s";
-$lang['fbmessage_link'] = "match/view/%s";
+$lang['fbmessage_link'] = "match/view/%1s";
 $lang['fbmessage_icon'] = "assets/images/sponsors/shareicons/%1s_%2s_shareicon.gif";
 
 $lang['__last_updated'] = '1318234635';
--- application/language/ko/fbmessage_lang.php
+++ application/language/ko/fbmessage_lang.php
@@ -13,7 +13,7 @@ $lang['fbmessage_caption_plural'] = "%1s가 %3s 경기에서 %2s에 대항하여
 
 $lang['fbmessage_title'] = "%1s배 %2s경기";
 $lang['fbmessage_score'] = "최종 점수: %1s %2s - %3s %4s";
-$lang['fbmessage_link'] = "match/view/%s";
+$lang['fbmessage_link'] = "match/view/%1s";
 $lang['fbmessage_icon'] = "assets/images/sponsors/shareicons/%1s_%2s_shareicon.gif";
 
 $lang['__last_updated'] = '1318234635';
