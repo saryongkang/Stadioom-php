7d1305af44275ed2e8863944208cb8e6ca96f7dc
Norú Moreno <noruisc@gmail.com>
2011-10-12 18:05:31 +0900

    Spanish fb messages updated!

--- application/language/es/fbmessage_lang.php
+++ application/language/es/fbmessage_lang.php
@@ -1,17 +1,17 @@
 <?php
 
-$lang['fbmessage_won_singular'] = "Gané!";
-$lang['fbmessage_won_plural'] = "Hemos ganado!";
-$lang['fbmessage_lost_singular'] = "Gran juego!";
-$lang['fbmessage_lost_plural'] = "Gran juego!";
-$lang['fbmessage_tied_singular'] = "Gran juego!";
-$lang['fbmessage_tied_plural'] = "Gran juego!";
+$lang['fbmessage_won_singular'] = "¡Gané el partido!";
+$lang['fbmessage_won_plural'] = "¡Ganamos el partido!";
+$lang['fbmessage_lost_singular'] = "Perdí...";
+$lang['fbmessage_lost_plural'] = "Perdimos...";
+$lang['fbmessage_tied_singular'] = "El partido quedó empatado";
+$lang['fbmessage_tied_plural'] = "Empatamos";
 
-$lang['fbmessage_caption_singular'] = "%1s defeated %2s in a fierce %3s match.";
-$lang['fbmessage_caption_plural'] = "%1s defeated %2s in a fierce %3s match.";
+$lang['fbmessage_caption_singular'] = "%1s venció a %2s en un intenso partido de %3s.";
+$lang['fbmessage_caption_plural'] = "%1s vencieron a %2s en un intenso partido de %3s.";
 
-$lang['fbmessage_title'] = "%1s %2s Partido";
-$lang['fbmessage_score'] = "La puntuación final: %1s %2s - %3s %4s";
+$lang['fbmessage_title'] = "Partido de %1s %2s";
+$lang['fbmessage_score'] = "Puntuación final: %1s %2s - %3s %4s";
 $lang['fbmessage_link'] = "http://stadioom.com/match/view/%s";
 $lang['fbmessage_icon'] = "http://stadioom.com/assets/images/sponsors/shareicons/%1s_%2s_shareicon.gif";
 
