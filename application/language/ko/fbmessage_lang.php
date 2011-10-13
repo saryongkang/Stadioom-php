9339fe2f22d59990ab220d45d8ca063712ad8211
Wegra <wegra@seedshock.com>
2011-10-14 05:43:40 +0900

    Updated i18n messages (for Korean)

--- application/language/ko/fbmessage_lang.php
+++ application/language/ko/fbmessage_lang.php
@@ -1,19 +1,28 @@
 <?php
-$lang['fbmessage_didntplay'] = "Amazing match!";
+$lang['fbmessage_didntplay'] = "정말 멋진 경기였어!";
 
-$lang['fbmessage_won_singular'] = "승리!";
-$lang['fbmessage_won_plural'] = "승리!";
-$lang['fbmessage_lost_singular'] = "패배!";
-$lang['fbmessage_lost_plural'] = "패배!";
-$lang['fbmessage_tied_singular'] = "무승부!";
-$lang['fbmessage_tied_plural'] = "무승부!";
+$lang['fbmessage_won_singular'] = "실력을 더 쌓고 다시 도전하시지!";
+$lang['fbmessage_won_plural'] = "역시 우리팀이 최고!";
+$lang['fbmessage_lost_singular'] = "내가 지다니...";
+$lang['fbmessage_lost_plural'] = "다음엔 이길거야...";
+$lang['fbmessage_tied_singular'] = "제법인데! 다음엔 꼭 승부를 가리자!";
+$lang['fbmessage_tied_plural'] = "오랫만에 적수를 만났군!";
 
-$lang['fbmessage_caption_singular'] = "%1s가 %3s 경기에서 %2s에 대항하여 승리했습니다.";
-$lang['fbmessage_caption_plural'] = "%1s가 %3s 경기에서 %2s에 대항하여 승리했습니다.";
+$lang['fbmessage_caption_won_singular'] = "%1\$s(이)가 %3\$s 경기에서 %2\$s를 눌렀습니다.";
+$lang['fbmessage_caption_won_plural'] = "%1\$s팀이 %3\$s 경기에서 %2\$s팀보다 한 수 위임을 증명했습니다.";
 
-$lang['fbmessage_title'] = "%1s배 %2s경기";
-$lang['fbmessage_score'] = "최종 점수: %1s %2s - %3s %4s";
-$lang['fbmessage_link'] = "match/view/%1s";
-$lang['fbmessage_icon'] = "assets/images/sponsors/shareicons/%1s_%2s_shareicon.gif";
+$lang['fbmessage_caption_lost_singular'] = "%1\$s이가 %2\$s(으)로부터 %3\$s에 대해 한 수 가르침을 받았습니다.";
+$lang['fbmessage_caption_lost_plural'] = "%3\$s로 맞붙으면 %1\$s팀은 아직 %2\$s팀에게는 무리로군요.";
+
+$lang['fbmessage_caption_tied_singular'] = "치열한 %3\$s 경기였지만 맞수 %1\$s와(과) %2\$s는 이번에도 승부를 가리지 못했습니다.";
+$lang['fbmessage_caption_tied_plural'] = "%1\$s팀과 %2\$s팀이 보기 드문 명승부를 펼쳤습니다. 최고의 %3\$s 경기였습니다.";
+
+$lang['fbmessage_andOthers'] = "%1\$s & %2\$d+";
+
+
+$lang['fbmessage_title'] = "%1\$s 배 %2\$s 대회";
+$lang['fbmessage_score'] = "최종 점수: %1\$s %2\$s - %3\$s %4\$s";
+$lang['fbmessage_link'] = "match/view/%1\$s";
+$lang['fbmessage_icon'] = "assets/images/sponsors/shareicons/%1\$s_%2\$s_shareicon.gif";
 
 $lang['__last_updated'] = '1318234635';
