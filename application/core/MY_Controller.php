a473821fc94d8dc96320ce1d5f26484f8e1e8c26
Norú Moreno <noruisc@gmail.com>
2011-10-14 10:25:28 +0900

    SetCookie instead of verify_csrf()

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -37,6 +37,39 @@ class FBAuth_Controller extends MY_Controller{
         
     }
     
+    private function _checkFbRequests(){
+        $facebook = $this->fb_connect;
+
+       //Assuming the user has already authenticated the app
+       $user_id = $facebook->getUser();
+
+       //get the request ids from the query parameter
+       $request_ids = explode(',', $_REQUEST['request_ids']);
+
+       //build the full_request_id from request_id and user_id 
+       function build_full_request_id($request_id, $user_id) {
+          return $request_id . '_' . $user_id; 
+       }
+
+       //for each request_id, build the full_request_id and delete request  
+       foreach ($request_ids as $request_id)
+       {
+          echo ("reqeust_id=".$request_id."<br>");
+          $full_request_id = build_full_request_id($request_id, $user_id);  
+          echo ("full_reqeust_id=".$full_request_id."<br>");
+
+          try {
+             $delete_success = $facebook->api("/$full_request_id",'DELETE');
+             if ($delete_success) {
+                echo "Successfully deleted " . $full_request_id;}
+             else {
+               echo "Delete failed".$full_request_id;}
+            }          
+          catch (FacebookApiException $e) {
+          echo "error";}
+        }
+    }
+    
     private function _login(){
         $this->load->helper('url');
         
